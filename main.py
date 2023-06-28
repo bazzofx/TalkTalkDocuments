4e18073f4511f194e7c8dc713f0da049a751b129
Paulo Bazzo <bazzofx@outlook.com>
2023-06-29 00:08:51 +0100

    final app working v1.0

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import HuggingFaceHub
 from htmlTemplates import css, bot_template, user_template
 
-def get_pdf_text(pdf_docs):
+def get_pdf_text(pdf_docs): #extract pdf pages from all .pdfs
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
@@ -18,7 +18,7 @@ def get_pdf_text(pdf_docs):
             text+= page.extract_text()
     return text
 
-def get_text_chunks(text):
+def get_text_chunks(text): #create chunks from raw pdf pages
     text_spliter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -29,15 +29,15 @@ def get_text_chunks(text):
     return chunks
 
     
-def get_vectorstore(text_chunks):
+def get_vectorstore(text_chunks): #make the embeddings from the chunks
     embeddings = OpenAIEmbeddings()
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore    
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    #llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.4, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -48,7 +48,16 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
     
-
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question':user_question})
+    st.session_state.chat_history = response['chat_history']
+    
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
+    
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with your PDFs ", page_icon="🙄")
@@ -57,9 +66,14 @@ def main():
     
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
         
     st.header("Chat with your files")
-    st.text_input("Ask a question about your documents.")
+    user_question = st.text_input("Ask a question about your documents.")
+    if user_question:
+        handle_userinput(user_question)
+       
     
     st.write(user_template.replace("{{MSG}}","Hello robot"), unsafe_allow_html=True)
     st.write(bot_template.replace("{{MSG}}","Hello there, upload your file on the left and ask me a question, if you dare.."), unsafe_allow_html=True)
@@ -73,7 +87,6 @@ def main():
                 
                 #get pdf chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
                 
